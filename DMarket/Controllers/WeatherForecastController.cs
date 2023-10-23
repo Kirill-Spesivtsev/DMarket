b3a09041fe7051a5e9541dfaaea66a5680da6404
Kirill-Spesivtsev <ksby8819@gmail.com>
2023-10-23 19:47:38 +0300

    Configured packages, DB connection

--- DMarket/Controllers/WeatherForecastController.cs
+++ DMarket/Controllers/WeatherForecastController.cs
@@ -4,10 +4,9 @@ using Microsoft.Identity.Web.Resource;
 
 namespace DMarket.Controllers
 {
-    [Authorize]
+
     [ApiController]
     [Route("[controller]")]
-    [RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
---
+++ DMarket/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DMarket.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+   
+        }
+
+    }
+}
