307c755f8da1c4cfc9d7c1fd124d2e18c19dbe1f
Kirill-Spesivtsev <ksby8819@gmail.com>
2023-11-06 17:38:08 +0300

    Added basic controller with model

---
+++ DMarket/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DMarket.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProduct()
+        {
+            return String.Empty;
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return String.Empty;
+        }
+    }
+}
