89aeec17dd412ba6971dfeb482009d80971da47f
Kirill-Spesivtsev <ksby8819@gmail.com>
2023-12-31 02:18:00 +0300

    Add shop router, configure lazy loading for it

--- DMarket.Client/src/app/app-routing.module.ts
+++ DMarket.Client/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailsComponent } from './shop/product-details/product-details.component';
 import { ShopComponent } from './shop/shop.component';
+import { ShopModule } from './shop/shop.module';
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
-  {path: "shop", component: ShopComponent},
-  {path: "shop/:id", component: ProductDetailsComponent},
+  {path: "shop", loadChildren: () => import("./shop/shop.module").then(m => m.ShopModule)},
+//{path: "shop/:id", component: ProductDetailsComponent},
   {path: "**", redirectTo: '', pathMatch: 'full'},
 ];
 
