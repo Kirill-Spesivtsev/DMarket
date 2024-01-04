9ab07196933daa5cba0c3b4674928dc867717673
Kirill-Spesivtsev <ksby8819@gmail.com>
2024-01-04 22:19:55 +0300

    Add JWT Token service in API

---
+++ DMarket.Application/Services/TokenService.cs
@@ -0,0 +1,48 @@
+﻿using DMarket.Application.Abstractions;
+using DMarket.Core.Entities.Identity;
+using Microsoft.Extensions.Configuration;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net;
+
+namespace DMarket.Application.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration _configuration;
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string CreateToken(ApplicationUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.GivenName, user.DisplayName)
+            };
+
+            var key = Encoding.UTF8.GetBytes(_configuration["JwtTokenOrigin:Key"]!);
+
+            var credentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddHours(4),
+                Issuer = _configuration["JwtTokenOrigin:Issuer"],
+                Audience = _configuration["JwtTokenOrigin:Audience"],
+                SigningCredentials = credentials
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
