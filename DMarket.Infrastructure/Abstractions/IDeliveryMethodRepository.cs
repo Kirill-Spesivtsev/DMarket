38147a07b536616c65ebd862d990a26168e807c8
Kirill-Spesivtsev <ksby8819@gmail.com>
2024-02-19 22:21:42 +0300

    Add DeliveryMethrodRepository

---
+++ DMarket.Infrastructure/Abstractions/IDeliveryMethodRepository.cs
@@ -0,0 +1,25 @@
+﻿using DMarket.Core.Entities.Order;
+using DMarket.Core.Entities;
+using DMarket.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMarket.Infrastructure.Abstractions
+{
+    public interface IDeliveryMethodRepository
+    {
+        public IQueryable<DeliveryMethod> GetAllDeliveryMethodsAsync();
+ 
+        public Task<DeliveryMethod?> GetDeliveryMethodByIdAsync(Guid id);
+
+        public Task AddDeliveryMethod(DeliveryMethod entity);
+  
+        public Task UpdateDeliveryMethod(DeliveryMethod entity);
+
+        public Task RemoveDeliveryMethod(DeliveryMethod entity);
+    }
+}
---
+++ DMarket.Infrastructure/Repositories/DeliveryMethodRepository.cs
@@ -0,0 +1,62 @@
+﻿using DMarket.Core.Entities;
+using DMarket.Core.Entities.Order;
+using DMarket.Infrastructure.Abstractions;
+using DMarket.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMarket.Infrastructure.Repositories
+{
+    public class DeliveryMethodRepository : IDeliveryMethodRepository, IDisposable
+    {
+        private readonly MarketDbContext _context;
+  
+        public DeliveryMethodRepository(MarketDbContext context)
+        {
+            _context = context;
+        }  
+        public IQueryable<DeliveryMethod> GetAllDeliveryMethodsAsync()  
+        {  
+            return _context.DeliveryMethods.AsNoTracking();  
+        }
+ 
+        public async Task<DeliveryMethod?> GetDeliveryMethodByIdAsync(Guid id) => await _context.DeliveryMethods.FindAsync(id);
+        
+        public async Task AddDeliveryMethod(DeliveryMethod entity)
+        {
+            _context.DeliveryMethods.Add(entity); 
+            await _context.SaveChangesAsync();
+        }
+  
+        public async Task UpdateDeliveryMethod(DeliveryMethod entity)
+        {
+            _context.DeliveryMethods.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveDeliveryMethod(DeliveryMethod entity)
+        { 
+            _context.DeliveryMethods.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed && disposing)
+            {
+                _context?.Dispose();
+            }
+            disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
