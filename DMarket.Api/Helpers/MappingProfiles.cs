2fa64a90c8c0f092f8604f91deeb33934c7d41b9
Kirill-Spesivtsev <ksby8819@gmail.com>
2024-03-09 23:09:45 +0300

    Add automapper profiles

--- DMarket.Api/Helpers/MappingProfiles.cs
+++ DMarket.Api/Helpers/MappingProfiles.cs
@@ -2,6 +2,9 @@ using AutoMapper;
 using DMarket.Api.DTO;
 using DMarket.Core.Entities;
 using DMarket.Core.Entities.Identity;
+using DMarket.Core.Entities.Order;
+using DMarket.Application.Dto;
+using DMarket.Application.DTO;
 
 namespace DMarket.Api.Helpers
 {
@@ -13,8 +16,18 @@ namespace DMarket.Api.Helpers
                 .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
                 .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
                 .ForMember(d => d.ImageUrl, o => o.MapFrom<ImageUrlResolver>());
-
             CreateMap<Address, AddressDto>().ReverseMap();
+            CreateMap<CustomerBasketDto, CustomerBasket>();
+            CreateMap<BasketItemDto, BasketItem>();
+            CreateMap<AddressDto, DeliveryAddress>();
+            CreateMap<Order, OrderToReturnDto>()
+                .ForMember(d => d.DeliveryMethod, o => o.MapFrom(s => s.DeliveryMethod.ShortName))
+                .ForMember(d => d.ShippingPrice, o => o.MapFrom(s => s.DeliveryMethod.Price));
+            CreateMap<OrderItem, OrderItemDto>()
+                .ForMember(d => d.ProductId, o => o.MapFrom(s => s.ItemOrdered.ProductItemId))
+                .ForMember(d => d.ProductName, o => o.MapFrom(s => s.ItemOrdered.ProductName))
+                .ForMember(d => d.ImageUrl, o => o.MapFrom(s => s.ItemOrdered.ImageUrl))
+                .ForMember(d => d.ImageUrl, o => o.MapFrom<OrderItemUrlResolver>());
         }
     }
 }
