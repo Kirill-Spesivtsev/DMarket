9c8f72a2c4c79e6fcd369c47d3a47ff02b6b03d4
Kirill-Spesivtsev <ksby8819@gmail.com>
2024-03-09 20:29:22 +0300

    Add UnitOfWork pattern

---
+++ DMarket.Infrastructure/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace DMarket.Infrastructure.Abstractions
+{
+    public interface IUnitOfWork
+    {
+        public Task SaveChangesAsync(CancellationToken cancellationToken = default);
+
+        public IDbTransaction BeginTransaction(IsolationLevel isolationLevel = IsolationLevel.ReadCommitted);
+    }
+}
---
+++ DMarket.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,68 @@
+﻿using DMarket.Infrastructure.Abstractions;
+using DMarket.Infrastructure.Data;
+using DMarket.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMarket.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly MarketDbContext _context;
+        
+        public IBasketRepository Baskets { get; private set; }
+        public IOrderRepository Orders { get; private set; }
+        public IDeliveryMethodRepository DeliveryMethods { get; private set; }
+        public IProductRepository Products { get; private set; }
+
+
+        public UnitOfWork(MarketDbContext context, IBasketRepository basketRepository)
+        {
+            _context = context;
+
+            Baskets = basketRepository;
+            Orders = new OrderRepository(_context);
+            DeliveryMethods = new DeliveryMethodRepository(_context);
+            Products = new ProductRepository(_context);
+
+        }
+ 
+        public async Task SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public IDbTransaction BeginTransaction(IsolationLevel isolationLevel = IsolationLevel.ReadCommitted)
+        {
+            var transaction = _context.Database.BeginTransaction();
+            return transaction.GetDbTransaction();
+        }
+
+        private bool disposed;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+                disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
